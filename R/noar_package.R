40801581a3dbbcd63c8c6580766a99313b32f58b
Konstantinos Vantas <kon.vantas@gmail.com>
2018-01-17 13:05:26 +0200

    Initial commit

---
+++ R/noar_package.R
@@ -0,0 +1,12 @@
+#' noar: Interface to National Observatory of Athens weather stations network
+#'
+#' R interface to the National Observatory of Athens weather stations data
+#'
+#' @section Data Sources:
+#'
+#' The data are retrieved from http://www.meteo.gr/
+#'
+#' @name noar
+#' @aliases noar-package
+#' @docType package
+"_PACKAGE"
---
+++ README.md
@@ -0,0 +1,6 @@
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+noar
+====
+
+R interface to the National Observatory of Athens weather stations
+[data](www.meteo.gr)
