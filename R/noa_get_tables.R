11374ac43fe621c80bc83506ef253aa2694bc084
Konstantinos Vantas <kon.vantas@gmail.com>
2018-01-19 14:29:40 +0200

    use hhtr for error handling

--- R/noa_get_tables.R
+++ R/noa_get_tables.R
@@ -11,14 +11,14 @@
 #' noa_get_latest()
 noa_get_latest <- function() {
 
-  noa_url <- "http://www.meteo.gr/observationsFull.cfm"
+  noa_path <- "observationsFull.cfm"
+  noa_url <- httr::modify_url("http://www.meteo.gr/", path = noa_path)
 
-  # TODO error hadling
-  # TODO translations
+  test_url(noa_url)
 
   html <- xml2::read_html(noa_url, encoding = "UTF-8")
   tables<- rvest::html_nodes(html, "table")
   obs_table <- rvest::html_table(tables[1], fill = TRUE)[[1]]
-
   tibble::as.tibble(obs_table)
+
 }
---
+++ R/utilities.R
@@ -0,0 +1,17 @@
+# use GET to test availibility of an url from meteo.gr
+#' @noRd
+test_url <- function(url) {
+
+  get_res <- tryCatch({
+    httr::GET(url)},
+    error = function(e){
+      stop("The server for that data source is probably down, get more info at
+           http://www.meteo.gr/contact.cfm or try again later.", call. = FALSE)
+    })
+
+  if (get_res$status_code != 200) {
+    stop("The server for that data source did not respond successfully.", call. = FALSE)
+  }
+
+  return(NULL)
+}
