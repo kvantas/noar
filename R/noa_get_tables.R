2fbd77a2385b27a2964b8258e0c10290514962ae
Konstantinos Vantas <kon.vantas@gmail.com>
2018-01-25 09:17:57 +0200

    remove links code

--- R/noa_get_tables.R
+++ R/noa_get_tables.R
@@ -16,21 +16,11 @@ noa_get_latest <- function() {
 
   test_url(noa_url)
 
-  ## read links
-  links <- noa_url magrittr::`%>%`
-    rvest::html_nodes("td:nth-child(1) a") %>% html_attr("href")
-  links <- links[grep("http", links)]
-
   ## read tables
   html <- xml2::read_html(noa_url, encoding = "UTF-8")
   tables <- rvest::html_nodes(html, "table")
   obs_table <- rvest::html_table(tables[1], fill = TRUE)[[1]]
   obs_table <- trasnlit_all(obs_table)
-
-  ## read links
-  links_nodes <- html_nodes("td:nth-child(1) a")
-  links <- html_attr(links_nodes, "href")
-  links <- links[grep("http", links)]
 
   ## translate columns names
   if (length(names(obs_table)) == 10){
@@ -39,12 +29,7 @@ noa_get_latest <- function() {
                         "temp_low", "temp_high", "precipitation")
   }
 
-  ## translit and add links
-  obs_table <- trasnlit_all(obs_table)
-
-  if(length(links) == NROW(obs_table)) {
-    obs_table$urls <- links
-  }
-  tibble::as.tibble(obs_table)
+  ## translit
+  tibble::as.tibble(trasnlit_all(obs_table))
 
 }
